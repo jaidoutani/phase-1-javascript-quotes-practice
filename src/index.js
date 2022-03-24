a31be822263f1318ca4a9c90b245c7a3286f20bc
Michael Bowlin <jaidoutani@gmail.com>
2022-03-23 19:06:12 -0500

    incomplete

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
+const quoteList = document.getElementById("quote-list")
+const newQuoteForm = document.getElementById("new-quote-form")
+const likeBtn = document.getElementsByClassName("btn-success")
+const deleteBtn = document.getElementsByClassName("btn-danger")
+const submitBtn = document.getElementsByClassName("btn btn-primary")
+
 document.addEventListener("DOMContentLoaded", () => {
     fetchQuotes()
+    newQuoteForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        processDOM(addQuoteCard)
+        // console.log()
+        newQuoteForm.reset()
+    })
+
 })
 function fetchQuotes() {
-fetch("http://localhost:3000/quotes?_embed=likes")
+return fetch("http://localhost:3000/quotes?_embed=likes")
     .then((resp) => resp.json())
     .then((data) => {
         data.forEach((quoteData) => {
+            // console.log(quoteData)
             processDOM(quoteData)
         })
     })
 }
 function processDOM(quoteData) {
-let quoteList = document.getElementById("quote-list")
-quoteList.innerHTML = 
+quoteList.innerHTML += 
     `<li class='quote-card'>
         <blockquote class="blockquote">
             <p class="mb-0">${quoteData.quote}</p>
@@ -23,4 +36,27 @@ quoteList.innerHTML =
         </blockquote>
     </li>
     `
+    for(let btn of likeBtn){
+        btn.addEventListener("click", addLikes)
+    }
+    for(let btn of deleteBtn){
+        btn.addEventListener("click", handleDelete)
+    }
+}
+function handleDelete(e) {
+    e.target.parentNode.remove()
+}
+function addQuoteCard(quote) {
+    let liTag = document.createElement("li")
+    liTag.className = "quote-card"
+    liTag.innerText = quote
+    quoteList.appendChild(liTag)
+}
+function addLikes(e) {
+    let spans = document.getElementsByTagName("span");
+    for(i = 0; i < spans.length; i++) {
+        if (spans === 0) {
+            e.target.spans++
+        }
+    }
 }
