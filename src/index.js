076d01253eb761d3d808a5ff76fd09c275263588
Michael Bowlin <jaidoutani@gmail.com>
2022-03-25 19:20:40 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,62 +1,80 @@
-const quoteList = document.getElementById("quote-list")
+const quotesList = document.getElementById("quote-list")
 const newQuoteForm = document.getElementById("new-quote-form")
-const likeBtn = document.getElementsByClassName("btn-success")
-const deleteBtn = document.getElementsByClassName("btn-danger")
-const submitBtn = document.getElementsByClassName("btn btn-primary")
 
-document.addEventListener("DOMContentLoaded", () => {
-    fetchQuotes()
-    newQuoteForm.addEventListener("submit", (e) => {
-        e.preventDefault()
-        processDOM(addQuoteCard)
-        // console.log()
-        newQuoteForm.reset()
-    })
-
-})
-function fetchQuotes() {
-return fetch("http://localhost:3000/quotes?_embed=likes")
-    .then((resp) => resp.json())
-    .then((data) => {
-        data.forEach((quoteData) => {
-            // console.log(quoteData)
-            processDOM(quoteData)
+fetch("http://localhost:3000/quotes?_embed=likes")
+    .then(res => res.json())
+    .then((quotesArray) => {
+        quotesArray.forEach((quoteObj) => {
+            processDOM(quoteObj)
         })
     })
-}
-function processDOM(quoteData) {
-quoteList.innerHTML += 
-    `<li class='quote-card'>
-        <blockquote class="blockquote">
-            <p class="mb-0">${quoteData.quote}</p>
-            <footer class="blockquote-footer">${quoteData.author}</footer>
-            <br>
-            <button class='btn-success'>Likes: <span>0</span></button>
-            <button class='btn-danger'>Delete</button>
-        </blockquote>
-    </li>
-    `
-    for(let btn of likeBtn){
-        btn.addEventListener("click", addLikes)
-    }
-    for(let btn of deleteBtn){
-        btn.addEventListener("click", handleDelete)
-    }
-}
-function handleDelete(e) {
-    e.target.parentNode.remove()
-}
-function addQuoteCard(quote) {
+
+// Add event listener to submit btn
+newQuoteForm.addEventListener("submit", (e) => {
+    // Prevent page from refreshing
+    e.preventDefault()
+    let author = e.target["author"].value
+    let quoteData = e.target["new-quote"].value
+    fetch("http://localhost:3000/quotes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            author: author,
+            quote: quoteData
+        })
+    })
+    .then(res => res.json())
+    .then((newQuote) => {
+        newQuote.likes = []
+        processDOM(newQuote)
+    })
+})
+
+function processDOM(quoteObj) {
+    // Create outermost element
     let liTag = document.createElement("li")
     liTag.className = "quote-card"
-    liTag.innerText = quote
-    quoteList.appendChild(liTag)
-}
-function addLikes(e) {
-    let spans = document.getElementsByTagName("span");
-    for(i = 0; i < spans.length; i++) {
-        if (spans === 0) {
-            e.target.spans++
-        }
-    }
+    // Fill outermost element using innerHTML
+    liTag.innerHTML = 
+    `<blockquote class="blockquote">
+        <p class="mb-0">${quoteObj.quote}</p>
+        <footer class="blockquote-footer">${quoteObj.author}</footer>
+        <br>
+        <button class='btn-success'>Likes: <span>${quoteObj.likes.length}</span></button>
+        <button class='btn-danger'>Delete</button>
+    </blockquote>`
+    // Append outermost element to DOM
+    quotesList.append(liTag)
+    // Get elements from outerelement
+    let deleteBtn = liTag.querySelector(".btn-danger") 
+    let likeBtn = liTag.querySelector(".btn-success")
+    let likeSpan = liTag.querySelector("span")
+    // Add event listeners
+    deleteBtn.addEventListener("click", () => {
+        fetch(`http://localhost:3000/quotes/${quoteObj.id}`, {
+            method: "DELETE"
+        })
+        .then(res => res.json())
+        .then(() => {
+            liTag.remove()
+        })
+    })
+    likeBtn.addEventListener("click", () => {
+        fetch("http://localhost:3000/likes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                quoteId: quoteObj.id
+            })
+        })
+        .then((res) => res.json())
+        .then((newLike) => {
+            quoteObj.likes.push(newLike)
+            likeSpan.innerText = quoteObj.likes.length
+        })
+    })
 }
